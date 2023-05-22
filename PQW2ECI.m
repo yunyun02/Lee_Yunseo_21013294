1a407c499d4d260f60fdaa901730868837c68d30
SEC <SEC@DESKTOP-AMV59HS>
2023-05-22 21:59:02 +0900

    for submission

---
+++ PQW2ECI.m
@@ -0,0 +1,15 @@
+function R = PQW2ECI(arg_prg, inc_angle, RAAN)
+%(radians)
+%arg_prg: Argument of perigee
+%inc_angle: inclination
+%RAAN: Right ascension of the ascending node
+
+R1=[cos(RAAN) -sin(RAAN) 0; sin(RAAN) cos(RAAN) 0; 0 0 1];
+
+R2 = [1 0 0; 0 cos(inc_angle) -sin(inc_angle); 0 sin(inc_angle) cos(inc_angle)];
+
+R3 = [cos(arg_prg) -sin(arg_prg) 0; sin(arg_prg) cos(arg_prg) 0; 0 0 1];
+
+R = R1*R2*R3;
+
+end
