8c822bd66bd736cc86eb819d627cb200e0fdf021
SEC <SEC@DESKTOP-AMV59HS>
2023-06-07 04:29:37 +0900

    project

---
+++ elevation.m
@@ -0,0 +1,12 @@
+function el= elevation(ENU, el_mask)
+
+for i = 1:size(ENU,1)
+    el(i) = asin(ENU(i,3)/norm(ENU(2,2)))*180/pi;
+
+    if el(i) < el_mask
+        el(i) = nan;
+    end
+
+end
+ 
+end
