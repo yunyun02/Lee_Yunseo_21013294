8e8b49de6b48d2d643ad005057ace8876b346ed4
SEC <SEC@DESKTOP-AMV59HS>
2023-06-21 22:13:45 +0900

    pro.2

--- elevation.m
+++ elevation.m
@@ -1,7 +1,9 @@
 function el= elevation(ENU, el_mask)
 
+el= asin (ENU(:,3)./sqrt(ENU(:,1).^2+ENU(:,2).^2+ENU(:,3).^2))*180/pi;
+
 for i = 1:size(ENU,1)
-    el(i) = asin(ENU(i,3)/norm(ENU(2,2)))*180/pi;
+   
 
     if el(i) < el_mask
         el(i) = NaN;
