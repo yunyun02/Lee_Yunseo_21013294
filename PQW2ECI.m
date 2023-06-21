72d2e53bcf803f9ee2686f4abf229229f61821c0
SEC <SEC@DESKTOP-AMV59HS>
2023-06-21 23:18:32 +0900

    pro.2

--- PQW2ECI.m
+++ PQW2ECI.m
@@ -4,6 +4,10 @@ function R = PQW2ECI(arg_prg, inc_angle, RAAN)
 %inc_angle: inclination
 %RAAN: Right ascension of the ascending node
 
+arg_prg= arg_prg*pi/180;
+inc_angle= inc_angle*pi/180;
+RAAN= RAAN*pi/180;
+
 R1=[cos(RAAN) -sin(RAAN) 0; sin(RAAN) cos(RAAN) 0; 0 0 1];
 
 R2 = [1 0 0; 0 cos(inc_angle) -sin(inc_angle); 0 sin(inc_angle) cos(inc_angle)];
@@ -11,5 +15,7 @@ R2 = [1 0 0; 0 cos(inc_angle) -sin(inc_angle); 0 sin(inc_angle) cos(inc_angle)];
 R3 = [cos(arg_prg) -sin(arg_prg) 0; sin(arg_prg) cos(arg_prg) 0; 0 0 1];
 
 R = R1*R2*R3;
+
+
 
 end
